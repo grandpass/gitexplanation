6c9b6a994cd0dc13a2ffff79f754522caa169a5d
grandpass <grandpass444@gmail.com>
2022-04-29 14:58:06 +0530

    first commit

---
+++ src/test/java/project/maven/oneTest.java
@@ -0,0 +1,15 @@
+package project.maven;
+
+import org.testng.annotations.Test;
+
+public class oneTest {
+
+	@Test
+	public void first() {
+		System.out.println("1");
+	}
+	@Test
+	public void second() {
+		System.out.println("1");
+	}
+}
